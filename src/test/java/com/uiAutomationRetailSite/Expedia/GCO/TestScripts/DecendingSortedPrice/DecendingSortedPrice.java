2f01587f1c634b695d2e3a054c5cb00690db7bce
Saurabh Sharma <saurabhsharma@Saurabhs-MacBook-Pro.local>
2019-08-26 21:00:51 -0500

    Adding the changes related to Finding product of lowest price..

--- src/test/java/com/uiAutomationRetailSite/Expedia/GCO/TestScripts/DecendingSortedPrice/DecendingSortedPrice.java
+++ src/test/java/com/uiAutomationRetailSite/Expedia/GCO/TestScripts/DecendingSortedPrice/DecendingSortedPrice.java
@@ -4,20 +4,16 @@ import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
 
-import org.apache.log4j.Logger;
 import org.openqa.selenium.WebElement;
-import org.testng.Assert;
 import org.testng.annotations.Test;
 
 import com.uiAutomationRetailSite.Expedia.GCO.Helper.Assertion.AssertionHelper;
 import com.uiAutomationRetailSite.Expedia.GCO.Helper.BrowserConfiguration.config.ObjectReader;
-import com.uiAutomationRetailSite.Expedia.GCO.Helper.Logger.LoggerHelper;
 import com.uiAutomationRetailSite.Expedia.GCO.PageObject.NavigationMenu.NavigationMenu;
 import com.uiAutomationRetailSite.Expedia.GCO.PageObject.ProductCategoryPages.ProductCategoryPage;
 import com.uiAutomationRetailSite.Expedia.GCO.TestBase.TestBase;
 
 public class DecendingSortedPrice extends TestBase {
-	private final static Logger log = LoggerHelper.getLogger(DecendingSortedPrice.class);
 
 	@Test(description = "Check the product of lowets price.")
 	public static void validateLowestPrice() throws InterruptedException {
@@ -31,32 +27,12 @@ public class DecendingSortedPrice extends TestBase {
 		Thread.sleep(8000);
 		// get price of all products
 		List<WebElement> allPrice = productCategoryPage.getAllProductsPrice();
-		//ArrayList<Integer> array = new ArrayList<Integer>();
+		// ArrayList<Integer> array = new ArrayList<Integer>();
 		Iterator<WebElement> itr = allPrice.iterator();
-		
+
 		ArrayList<Integer> data = productCategoryPage.getPriceMassagedData(itr);
 		boolean status = productCategoryPage.verifyArrayHasAscendingData(data);
 		AssertionHelper.updateTestStatus(status);
-		
-//		while (itr.hasNext()) {
-//			String p = itr.next().getText();
-//			// remove $ sign from price string before converting it to doubld.
-//			if (p.contains("$")) {
-//				String actaulData = p.substring(1);
-//				log.info(actaulData);
-//				double price = Double.parseDouble(actaulData);
-//				int productPrice = (int) (price);
-//				array.add(productPrice);
-//			}
-//		}
-//		log.info(array);
-//		for (int i = 0; i < array.size() - 1; i++) {
-//			if (array.get(i) <= array.get(i + 1)) {
-//				log.info("obj.get(i):-" + array.get(i));
-//				log.info("obj.get(i+1):-" + array.get(i + 1));
-//			} else {
-//				Assert.assertTrue(false, "Price filter is working fine.");
-//			}
-//		}
+
 	}
 }
